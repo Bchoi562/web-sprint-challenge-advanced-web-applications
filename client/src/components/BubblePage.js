a4e5dd98dfc5646e1698e608e065b265f0596614
Brian Choi <b.choi562@gmail.com>
2020-12-18 10:17:15 -0800

    sprint challenge

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-
+   
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
